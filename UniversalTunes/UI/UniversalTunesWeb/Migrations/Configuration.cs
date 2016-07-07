39603c692b11dd05908cd1258f655bb904080338
Innov8 <mvupha@gmail.com>
2016-07-07 10:01:10 +0200

    Projects created.

---
+++ UniversalTunes/UI/UniversalTunesWeb/Migrations/Configuration.cs
@@ -0,0 +1,46 @@
+namespace UniversalTunesWeb.Migrations
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<UniversalTunesWeb.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(UniversalTunesWeb.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+
+            var user = new ApplicationUser()
+            {
+                UserName = "admin@universaltunes.co.za",
+                Email = "admin@universaltunes.co.za",
+                EmailConfirmed = true,
+                Name = "James",
+                LastName = "Bond",
+                DateCreated = DateTime.Now.AddYears(-1)
+            };
+
+            manager.Create(user, "Pass@123");
+        }
+    }
+}
